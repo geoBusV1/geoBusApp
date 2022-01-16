5e106f012560502ac78bfb19444309ea636ec34a
rahulrao011 <rahul.seth.rao@gmail.com>
2022-01-15 22:53:31 -0600

    Rahul-UI homescreen done

--- lib/busLocationBusDriverView.dart
+++ lib/busLocationBusDriverView.dart
@@ -1,12 +1,126 @@
+import 'package:background_location/background_location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class BusLocationBusDriverView extends StatelessWidget {
+// implementing Ibrahim's Branch
+
+//testing change from desktop pc
+
+class BusLocationBusDriverView extends StatefulWidget {
+  @override
+  _BusLocationBusDriverViewState createState() => _BusLocationBusDriverViewState();
+}
+
+class _BusLocationBusDriverViewState extends State<BusLocationBusDriverView> {
+  Future<http.Response> fetchAlbum(query) {
+    return http.get(Uri.parse(
+        'https://geobus-server.ibrahimshah.repl.co/questions' + query));
+  }
+
+  String latitude = 'waiting...';
+  String longitude = 'waiting...';
+  String altitude = 'waiting...';
+  String accuracy = 'waiting...';
+  String bearing = 'waiting...';
+  String speed = 'waiting...';
+  String time = 'waiting...';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Test Bus Driver Screen'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Background Location Service'),
+        ),
+        body: Center(
+          child: ListView(
+            children: <Widget>[
+              locationData('Latitude: ' + latitude),
+              locationData('Longitude: ' + longitude),
+              locationData('Altitude: ' + altitude),
+              locationData('Accuracy: ' + accuracy),
+              locationData('Bearing: ' + bearing),
+              locationData('Speed: ' + speed),
+              locationData('Time: ' + time),
+              ElevatedButton(
+                  onPressed: () async {
+                    await BackgroundLocation.setAndroidNotification(
+                      title: 'Background service is running',
+                      message: 'Background location in progress',
+                      icon: '@mipmap/ic_launcher',
+                    );
+                    //await BackgroundLocation.setAndroidConfiguration(1000);
+                    await BackgroundLocation.startLocationService(
+                        distanceFilter: 20);
+                    BackgroundLocation.getLocationUpdates((location) {
+                      setState(() {
+                        latitude = location.latitude.toString();
+                        longitude = location.longitude.toString();
+                        accuracy = location.accuracy.toString();
+                        altitude = location.altitude.toString();
+                        bearing = location.bearing.toString();
+                        speed = location.speed.toString();
+                        time = DateTime.fromMillisecondsSinceEpoch(
+                                location.time!.toInt())
+                            .toString();
+                      });
+
+                      fetchAlbum("?latitude=$latitude&longitude=$longitude");
+
+                      /*print('''\n
+                        Latitude:  $latitude
+                        Longitude: $longitude
+                        Altitude: $altitude
+                        Accuracy: $accuracy
+                        Bearing:  $bearing
+                        Speed: $speed
+                        Time: $time
+                      ''');*/
+                    });
+                  },
+                  child: Text('Start Location Service')),
+              ElevatedButton(
+                  onPressed: () {
+                    BackgroundLocation.stopLocationService();
+                  },
+                  child: Text('Stop Location Service')),
+              ElevatedButton(
+                  onPressed: () {
+                    getCurrentLocation();
+                  },
+                  child: Text('Get Current Location')),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  Widget locationData(String data) {
+    return Text(
+      data,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 18,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  void getCurrentLocation() {
+    BackgroundLocation().getCurrentLocation().then((location) {
+      print('This is current Location ' + location.toMap().toString());
+    });
+  }
+
+  @override
+  void dispose() {
+    BackgroundLocation.stopLocationService();
+    super.dispose();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'busLocationBusDriverView.dart';
 import 'busLocationStudentView.dart';
+import 'busLocationBusDriverView.dart';
 
 void main() => runApp(const MyApp());
 
